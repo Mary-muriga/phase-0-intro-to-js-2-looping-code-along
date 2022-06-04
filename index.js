d44a11fe6bf13a887cbbb9ead6cfb78a0eef9908
Marymuriga <marymuriga@gmail.com>
2022-06-04 14:29:46 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solutions in this file
+function writeCards([name1, name2, nam3], event){
+    return writeCards = [
+        "Thank you, Guadalupe, for the wonderful surprise gift!",
+        "Thank you, Ollie, for the wonderful surprise gift!",
+        "Thank you, Aki, for the wonderful surprise gift!",
+    ]
+    
+    debugger;
+
+}
+const num = 10;
+function countDown(num){
+    while(num >= 0){
+        console.log(num)
+        num--
+    }
+    return num;
+}
